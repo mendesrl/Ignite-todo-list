68f9f28cfd128176b94763c4cd9cbab2a8163c87
mendesrl <ribeiro.larissamendes@gmail.com>
2023-02-12 12:29:54 -0300

    feat: add and import TaskBoard component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import {Header} from './components/Header'
 import { NewTask } from './components/NewTask'
+import { TaskBoard } from './components/TaskBoard'
 export function App() {
 
   return (
     <div className="App">
         <Header />
         <NewTask />
+        <TaskBoard />
     </div>
   )
 }
---
+++ src/components/TaskBoard.tsx
@@ -0,0 +1,11 @@
+export function TaskBoard() {
+  return (
+    <main>
+      <div>Tarefas criadas 0 Concluídas 0</div>
+      <div>
+        Você ainda não tem tarefas cadastradas Crie tarefas e organize seus
+        itens a fazer
+      </div>
+    </main>
+  );
+}
