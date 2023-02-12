47e9ba7d2090173cf8678266cdaaebde09f731d1
mendesrl <ribeiro.larissamendes@gmail.com>
2023-02-12 12:31:45 -0300

    fix: formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
-import {Header} from './components/Header'
-import { NewTask } from './components/NewTask'
-import { TaskBoard } from './components/TaskBoard'
+import { Header } from "./components/Header";
+import { NewTask } from "./components/NewTask";
+import { TaskBoard } from "./components/TaskBoard";
 export function App() {
-
   return (
     <div className="App">
-        <Header />
-        <NewTask />
-        <TaskBoard />
+      <Header />
+      <NewTask />
+      <TaskBoard />
     </div>
-  )
+  );
 }
-
