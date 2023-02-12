e7124cd17b11e618373384d8f120673f0b98f72a
mendesrl <ribeiro.larissamendes@gmail.com>
2023-02-12 12:25:36 -0300

    feat: add new component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import {Header} from './components/Header'
+import { NewTask } from './components/NewTask'
 export function App() {
 
   return (
     <div className="App">
         <Header />
+        <NewTask />
     </div>
   )
 }
