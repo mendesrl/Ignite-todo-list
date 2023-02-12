7531169c2394e648d94d6d052770fa53f66d6e7e
mendesrl <ribeiro.larissamendes@gmail.com>
2023-02-12 19:43:52 -0300

    feat: add component

--- src/components/TaskBoard.tsx
+++ src/components/TaskBoard.tsx
@@ -1,8 +1,30 @@
+import "./TaskBoard.modules.css";
+import { ClipboardText } from "phosphor-react";
+
 export function TaskBoard() {
   return (
-    <main>
-      <div>Tarefas criadas 0 Concluídas 0</div>
-      <div>
+    <main className="task-board">
+      <div className="task-status">
+        <div>
+          <span className="task-status__text task-status__text--blue">
+            Tarefas criadas
+          </span>
+          <div className="task-status__badge">
+            <span className="task-status__number">0</span>
+          </div>
+        </div>
+        <div>
+          <span className="task-status__text task-status__text--purple">
+            Concluídas
+          </span>
+          <div className="task-status__badge">
+            <span className="task-status__number">0</span>
+          </div>
+        </div>
+      </div>
+      
+      <div className="tasks">
+      <ClipboardText size={56} weight="thin" />
         Você ainda não tem tarefas cadastradas Crie tarefas e organize seus
         itens a fazer
       </div>
