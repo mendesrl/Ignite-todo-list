64bbfb59cb0d3a02f6c64c29807608292c779ec6
mendesrl <ribeiro.larissamendes@gmail.com>
2023-02-14 09:35:26 -0300

    add lint

--- src/components/TaskBoard.tsx
+++ src/components/TaskBoard.tsx
@@ -1,5 +1,5 @@
 import "./TaskBoard.modules.css";
-import { ClipboardText } from "phosphor-react";
+import { ClipboardText, Trash } from "phosphor-react";
 
 export function TaskBoard() {
   return (
@@ -22,11 +22,42 @@ export function TaskBoard() {
           </div>
         </div>
       </div>
-      
+
+      <div className="task-board-empty">
+        <ClipboardText size={56} weight="thin" />
+        <p className="task-board-empty__text--bold">
+          Você ainda não tem tarefas cadastradas
+        </p>
+        <p>Crie tarefas e organize seus itens a fazer</p>
+      </div>
       <div className="tasks">
-      <ClipboardText size={56} weight="thin" />
-        Você ainda não tem tarefas cadastradas Crie tarefas e organize seus
-        itens a fazer
+        <div className="task">
+          <div className="task__title">
+            <input type="checkbox" id="checkbox-1" name="checkbox-1" checked />
+          </div>
+          <div className="task__description">
+            <span className="task__description__text">
+              Integer urna interdum massa libero auctor neque turpis turpis
+              semper. Duis vel sed fames integer.
+            </span>
+          </div>
+          <div className="task__actions">
+            <Trash size={14} weight="bold" />
+          </div>
+        </div>
+        <div className="task">
+          <div className="task__title">
+            <input type="checkbox" id="checkbox-2" name="checkbox-2" />
+          </div>
+          <div className="task__description">
+            <span className="task__description__text">
+              Integer.
+            </span>
+          </div>
+          <div className="task__actions">
+            <Trash size={18} weight="bold" />
+          </div>
+        </div>
       </div>
     </main>
   );
