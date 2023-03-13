08ed5b471e4f48cf71ec52462cb481cc6ca81379
mendesrl <ribeiro.larissamendes@gmail.com>
2023-03-13 13:14:26 -0400

    refatorando o compoenente task board

--- src/components/TaskBoard.tsx
+++ src/components/TaskBoard.tsx
@@ -3,78 +3,97 @@ import './TaskBoard.modules.css';
 import { ClipboardText } from 'phosphor-react';
 import { Task } from './Task';
 import { useState } from 'react';
+import { PlusCircle } from 'phosphor-react';
 
-
-export function TaskBoard(props:any) {
+export function TaskBoard(props: any) {
   //state : variavel que armazena o estado atual do componente (monitorada)
-  const [tasks,setTasks] = useState (
-    [
-      {
-        id: 1,
-        description: 'Task 1',
-        completed: true,
-      },
-      {
-        id: 2,
-        description: 'Task 2',
-        completed: false,
-      },
-    ]
-  )
+  const [tasks, setTasks] = useState([
+    // {
+    //   id: 2,
+    //   description: 'Task 2',
+    //   completed: false,
+    // },
+  ]);
+  const [newTask, setNewTask] = useState('');
   const isEmpty = tasks.length === 0;
+  const totalTasks = tasks.length;
+  const completedTasks = tasks.filter((task) => task.completed).length;
 
-  function recebeNovaTarefa() {
-    setTasks([
-    ...tasks,
-      props.newTask,
-    ])
+  function handleCreateNewTask() {
+    event.preventDefault();
+    const newDescriptionTask = {
+      id:3,
+      description: event.target.descriptionTask.value,
+      completed: false,
+    }
+    setTasks([...tasks, newDescriptionTask]);
+    event.target.descriptionTask.value = '';
   }
-  
+
+  // function handleDeleteTask(id) {
+  //   var
+  // }
+
   return (
-    <main className="task-board">
-      <div className="task-status">
-        <div>
-          <span className="task-status__text task-status__text--blue">
-            Tarefas criadas
-          </span>
-          <div className="task-status__badge">
-            <span className="task-status__number">0</span>
+    <>
+      <form onSubmit={handleCreateNewTask}>
+        <div className="new-task">
+          <input
+            name="descriptionTask"
+            className="new-task__input"
+            placeholder="Adicione uma nova tarefa"
+          ></input>
+          <button className="new-task__button" type="submit">
+            <span>Criar</span>
+            <PlusCircle size={18} weight="bold" />
+          </button>
+        </div>
+      </form>
+
+      <main className="task-board">
+        <div className="task-status">
+          <div>
+            <span className="task-status__text task-status__text--blue">
+              Tarefas criadas
+            </span>
+            <div className="task-status__badge">
+              <span className="task-status__number">{totalTasks}</span>
+            </div>
+          </div>
+          <div>
+            <span className="task-status__text task-status__text--purple">
+              Concluídas
+            </span>
+            <div className="task-status__badge">
+              <span className="task-status__number">{completedTasks}</span>
+            </div>
           </div>
         </div>
-        <div>
-          <span className="task-status__text task-status__text--purple">
-            Concluídas
-          </span>
-          <div className="task-status__badge">
-            <span className="task-status__number">0</span>
+
+        {isEmpty && (
+          <div className="task-board-empty">
+            <ClipboardText size={56} weight="thin" />
+            <p className="task-board-empty__text--bold">
+              Você ainda não tem tarefas cadastradas
+            </p>
+            <p>Crie tarefas e organize seus itens a fazer</p>
           </div>
-        </div>
-      </div>
+        )}
 
-      {isEmpty && (
-        <div className="task-board-empty">
-          <ClipboardText size={56} weight="thin" />
-          <p className="task-board-empty__text--bold">
-            Você ainda não tem tarefas cadastradas
-          </p>
-          <p>Crie tarefas e organize seus itens a fazer</p>
-        </div>
-      )}
-      <button onClick={recebeNovaTarefa}>teste</button>
-
-      {!isEmpty && (
-        <div className="tasks" >
-          {tasks.map((task) => {
-            return (
-              <Task
-                id={task.id}
-                description={task.description}
-                completed={task.completed}
-              />
-            );
-          })}
-        </div>
-      )}
-    </main>
+        {!isEmpty && (
+          <div className="tasks">
+            {tasks.map((task) => {
+              return (
+                <Task
+                  id={task.id}
+                  description={task.description}
+                  completed={task.completed}
+                />
+              );
+            })}
+          </div>
+        )}
+      </main>
+    </>
   );
 }
