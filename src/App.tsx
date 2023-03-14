5a0da3170c48af8b7e841a17c99df11fade35624
mendesrl <ribeiro.larissamendes@gmail.com>
2023-03-13 20:51:47 -0400

    remove component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import { Header } from './components/Header';
-import { NewTask } from './components/NewTask';
 import { TaskBoard } from './components/TaskBoard';
 export function App() {
   return (
     <div className="App">
       <Header />
-      <NewTask />
       <TaskBoard />
     </div>
   );
